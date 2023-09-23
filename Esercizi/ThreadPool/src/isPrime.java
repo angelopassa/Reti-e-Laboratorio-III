659ec7e4e1cc40b6dae5d0a3f243e322e12716cf
Angelo <passarelliangelo2.0@gmail.com>
2023-09-23 16:37:52 +0200

    Esericizio 1

---
+++ Esercizi/ThreadPool/src/isPrime.java
@@ -0,0 +1,22 @@
+public class isPrime implements Runnable {
+    private int n = 10000000;
+    @Override
+    public void run() {
+        int nPrimi = 0;
+        boolean flag;
+        for (int i = 1; i < n; i++){
+            flag = true;
+            for (int j = 2; j <= Math.floor(Math.sqrt(i)); j++){
+                if (i % j == 0){
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag){
+                //System.out.println(Thread.currentThread().getName() + ": " + "Trovato un numero primo = " + i);
+                nPrimi++;
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + ": Nr. di numeri primi minori di " + this.n + " = " + nPrimi);
+    }
+}
