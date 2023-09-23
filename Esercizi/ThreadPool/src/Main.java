14629542ebc4052dec94c984556a90d4fee0ca55
Angelo <passarelliangelo2.0@gmail.com>
2023-09-23 16:37:52 +0200

    Esericizio 1

---
+++ Esercizi/ThreadPool/src/Main.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.concurrent.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Numero di thread: ");
+        String input = keyboard.readLine();
+        int nThreads = 0;
+        try {
+            nThreads = Integer.parseInt(input);
+        }catch (Exception e){
+            System.out.println("Inserire un numero");
+        }
+
+        ExecutorService executor = null;
+        try {
+            executor = Executors.newFixedThreadPool(nThreads);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
+        System.out.print("Numero di task: ");
+        input = keyboard.readLine();
+        int nTasks = 0;
+        try {
+            nTasks = Integer.parseInt(input);
+        }catch (Exception e){
+            System.out.println("Inserire un numero");
+        }
+
+        for (int i = 0; i < nTasks; i++){
+            executor.submit(new isPrime());
+        }
+    }
+}
